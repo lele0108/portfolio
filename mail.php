74ecc534ed6866d97d90ebb82962a5754c887584
Jimmy Liu <sicong.liu98@gmail.com>
2013-03-18 00:04:27 -0700

    I is awesome

--- mail.php
+++ mail.php
@@ -6,5 +6,4 @@ $recipient = "sicong.liu98@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
-?>
+echo "I'll get back to you soon!";
